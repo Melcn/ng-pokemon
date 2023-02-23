8085bc84e3400babf778f89cce7e06aceca660a1
Melanie C <121883970+Melcn@users.noreply.github.com>
2023-02-23 11:58:43 +0100

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core'; // Import les elements dont on va avoir besoin (componenent angular)
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
 
   selector: 'app-root',
-  template: `<h1> Welcome to {{ pokemonList[1] }}! </h1>`
+  template: `<h1> Liste de pokemons </h1>`
 
 
 })
 
 export class AppComponent implements OnInit{
-  pokemonList = ['Bulbizarre', 'Salameche', 'Carapuce'];
+  pokemonList: Pokemon[] = POKEMONS;
 
   ngOnInit(){
     console.table(this.pokemonList);
+    this.selectPokemon(this.pokemonList[0]);
+  }
+
+  selectPokemon(pokemon: Pokemon) { //method pour selectionner pkm
+    console.log(`Vous avez cliqué sur le pokemon ${pokemon.name}`);
   }
 }
