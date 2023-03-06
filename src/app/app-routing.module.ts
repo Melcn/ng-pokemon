2dc42ba490f1577186e95fb4c9f45a8f45c15444
Melanie C <121883970+Melcn@users.noreply.github.com>
2023-03-06 11:34:17 +0100

    ng+pkm+path1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Les routes sont lu du haut vers le bas 
+  { path: 'pokemons', component: ListPokemonComponent },
+  { path: 'pokemon:id', component: DetailPokemonComponent},
+  { path: '', redirectTo: 'pokemons', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
