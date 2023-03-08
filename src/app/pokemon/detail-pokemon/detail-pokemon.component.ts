0f85bbc4a1e83fa87a74adfc921d3b51c12a7da6
Melanie C <121883970+Melcn@users.noreply.github.com>
2023-03-08 11:29:36 +0100

    ng+pkm+services

---
+++ src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
+
+@Component({
+  selector: 'app-detail-pokemon',
+  templateUrl: './detail-pokemon.component.html'
+})
+
+export class DetailPokemonComponent implements OnInit{
+
+  pokemonList!: Pokemon[];
+  pokemon: Pokemon|undefined;
+
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router,
+    private pokemonService: PokemonService
+    ) { }
+
+  ngOnInit() {
+   
+    const pokemonId: string|null = this.route.snapshot.paramMap.get('id');
+
+    if(pokemonId) {
+      this.pokemon = this.pokemonService.getPokemonById(+pokemonId);
+    }
+
+  }
+
+  goToPokemonList() {
+    this.router.navigate(['/pokemons']);
+  }
+}
