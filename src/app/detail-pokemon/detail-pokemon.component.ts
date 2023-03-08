f027e9c313e5b28aa990775b1190b05852b93f86
Melanie C <121883970+Melcn@users.noreply.github.com>
2023-03-08 09:50:27 +0100

    ng+pkm+path4

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,10 +1,7 @@
-
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { POKEMONS } from '../mock-pokemon-list';
 import { Pokemon } from '../pokemon';
-
-
 
 @Component({
   selector: 'app-detail-pokemon',
@@ -12,7 +9,6 @@ import { Pokemon } from '../pokemon';
   styles: [
   ]
 })
-
 export class DetailPokemonComponent implements OnInit{
 
   pokemonList!: Pokemon[];
