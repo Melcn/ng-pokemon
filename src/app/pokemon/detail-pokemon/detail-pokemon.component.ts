a104236a3fa58d00a89e1a89a724b2134079d7c5
Melanie C <121883970+Melcn@users.noreply.github.com>
2023-03-14 10:41:04 +0100

    httpRequest+delete

--- src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
+++ src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
@@ -30,6 +30,11 @@ export class DetailPokemonComponent implements OnInit{
 
   }
 
+  deletePokemon(pokemon: Pokemon) {
+    this.pokemonService.deletePokemonById(pokemon.id)
+    .subscribe(() => this.goToPokemonList());
+  }
+
   goToPokemonList() {
     this.router.navigate(['/pokemons']);
   }
--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -23,20 +23,24 @@ export class PokemonService {
   }
 
 
-
   updatePokemon(pokemon: Pokemon): Observable<null> {
-
     const httpOptions = {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' })
     };
 
     return this.http.put('api/pokemons', pokemon, httpOptions).pipe(
       tap((response) => this.log(response)),
-
       catchError((error) => this.handleError(error, null))
-
     )
   }
+
+  deletePokemonById(pokemonId: number): Observable<null>{
+    return this.http.delete(`api/pokemons/${pokemonId}`).pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, null))
+    );
+  }
+
 
 private log(response: any) {
   console.table(response);
