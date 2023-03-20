a6e296d5ddc6b7554fc5e52b48b1818cc3410c38
Melanie C <121883970+Melcn@users.noreply.github.com>
2023-03-20 15:04:09 +0100

    add+img

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -41,12 +41,12 @@ export class PokemonService {
     );
   }
 
-  addPokemon(pokemon: Pokemon): Observable<null> {
+  addPokemon(pokemon: Pokemon): Observable<Pokemon> {
     const httpOptions = {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' })
     };
 
-    return this.http.post('api/pokemons', pokemon, httpOptions).pipe(
+    return this.http.post<Pokemon>('api/pokemons', pokemon, httpOptions).pipe(
       tap((response) => this.log(response)),
       catchError((error) => this.handleError(error, null))
     );
